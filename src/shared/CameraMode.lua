eccceba99b3ac109d3dadd5b8697595b4b1536b6
Sean Hodges <sean.hodges@nserc-crsng.gc.ca>
2020-08-19 20:47:59 -0400

    Hook the camera mode into a client event

--- src/shared/CameraMode.lua
+++ src/shared/CameraMode.lua
@@ -1,14 +1,23 @@
 local CameraMode = { }
 
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Network = ReplicatedStorage:WaitForChild("Network")
+
 local Modes = {
     "PlayerView",
     "Overhead"
 }
 local currentMode = 1
 
+local cameraModeChangedEvt = Network:WaitForChild("CameraModeChanged")
+
 function CameraMode.Swap()
     currentMode = (currentMode % #Modes) + 1
-    print("Now in mode ", Modes[currentMode])
+    cameraModeChangedEvt:Fire(Modes[currentMode])
 end
+
+cameraModeChangedEvt.Event:Connect(function(mode)
+    print("The camera is now in ", mode)
+end)
 
 return CameraMode
