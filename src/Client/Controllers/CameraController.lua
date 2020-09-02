2cba0f5c4ce05d8a505c18e5c5cc67384a3f1b81
Sean Hodges <sean.hodges@nserc-crsng.gc.ca>
2020-09-01 22:57:06 -0400

    Add zooming to the overhead camera

--- src/Client/Controllers/CameraController.lua
+++ src/Client/Controllers/CameraController.lua
@@ -12,7 +12,10 @@ local CameraController = {}
 
 local RunService = game:GetService("RunService")
 local ContextActionService = game:GetService("ContextActionService")
+local UserInputService = game:GetService("UserInputService")
+local UserGameSettings = UserSettings():GetService("UserGameSettings")
 
+-- Camera mode data
 local CameraModes = {
     {
         ModeName = "PlayerView",
@@ -27,6 +30,7 @@ local CameraModes = {
 }
 
 -- Global Variables
+local zoom = 75
 local speed = 0.75
 local currentMode = 1
 local camera
@@ -36,10 +40,23 @@ local target
 local cameraPosition
 local maid
 
+-- Zoom Constants (taken from the BaseCamera code provided by Roblox)
+local PAN_SENSITIVITY = 20
+local MOUSE_SENSITIVITY = Vector2.new( 0.002 * math.pi, 0.0015 * math.pi )
+local ZOOM_SENSITIVITY_CURVATURE = 0.5
+local FFlagUserFixZoomInZoomOutDiscrepancy do
+    local success, result = pcall(function()
+        return UserSettings():IsUserFeatureEnabled("UserFixZoomInZoomOutDiscrepancy")
+    end)
+    FFlagUserFixZoomInZoomOutDiscrepancy = success and result
+end
+
+-- Retrieves humanoid for the active player
 local function GetHumanoid()
     return player.Character and player.Character:FindFirstChild("Humanoid")
 end
 
+-- Starts up the controller
 function CameraController:Start()
     -- Global Variable setup
     camera = workspace.CurrentCamera
@@ -61,30 +78,40 @@ function CameraController:Start()
         self:ChangeMode()
     end, true, Enum.KeyCode.LeftControl, Enum.KeyCode.ButtonR2)
     maid:GiveTask(function() ContextActionService:UnbindAction("SwapCameraMode") end)
+
+    -- Camera zoom
+    maid:GiveTask(UserInputService.PointerAction:Connect(function(wheel, pan, pinch, processed)
+        self:OnPointerAction(wheel, pan, pinch, processed)
+    end))
 end
 
-
+-- Called when the controller initializes
 function CameraController:Init()
     self:RegisterEvent("CameraModeChanged")
 end
 
+-- Cleanup
 function CameraController:Destroy()
     maid:DoCleaning()
 end
 
+-- Retrieves the name of the current mode (Overhead or PlayerView)
 function CameraController:GetMode()
     return self:GetModeData().ModeName
 end
+-- Retrieves full data for the current mode
 function CameraController:GetModeData()
     return CameraModes[currentMode]
 end
 
+-- Changes the active camera mode
 function CameraController:ChangeMode()
     currentMode = (currentMode % #CameraModes) + 1
     self:Apply()
     self:FireEvent("CameraModeChanged", self:GetMode())
 end
 
+-- Applies a new Camera Mode
 function CameraController:Apply()
     local data = self:GetModeData()
     player.CameraMode = data.CameraMode
@@ -93,7 +120,7 @@ function CameraController:Apply()
 
     if self:GetMode() == "Overhead" then
         local cf = camera.CFrame
-        cameraPosition = Vector3.new(cf.X, 75, cf.Z)
+        cameraPosition = Vector2.new(cf.X, cf.Z)
 
         controls:Disable()
     else
@@ -101,6 +128,7 @@ function CameraController:Apply()
     end
 end
 
+-- Updates Camera position when in overhead view
 function CameraController:Update()
     -- Only needs to apply any changes if the camera is in Overhead view
     if self:GetMode() ~= "Overhead" then
@@ -108,9 +136,39 @@ function CameraController:Update()
     end
 
     local moveVector = controls:GetMoveVector()
-    camera.CFrame = CFrame.new(cameraPosition,Vector3.new(cameraPosition.X,0,cameraPosition.Z))
+    camera.CFrame = CFrame.new(Vector3.new(cameraPosition.X, zoom, cameraPosition.Y), Vector3.new(cameraPosition.X,0,cameraPosition.Y))
     
-    cameraPosition += Vector3.new(-moveVector.Z * speed, 0, moveVector.X * speed)
+    cameraPosition += Vector2.new(-moveVector.Z * speed, moveVector.X * speed)
+end
+
+-- Taken from the Roblox BaseCamera module. Controls the zoom level.
+function CameraController:OnPointerAction(wheel, pan, pinch, processed)
+    if processed or self:GetMode() ~= "Overhead" then
+        return
+    end
+
+    if pan.Magnitude > 0 then
+        local inversionVector = Vector2.new(1, UserGameSettings:GetCameraYInvertValue())
+        local rotateDelta = self:InputTranslationToCameraAngleChange(PAN_SENSITIVITY*pan, MOUSE_SENSITIVITY)*inversionVector
+        self.rotateInput = self.rotateInput + rotateDelta
+    end
+
+    local zoomDelta = -(wheel + pinch)
+
+    if math.abs(zoomDelta) > 0 then
+        local newZoom
+        if FFlagUserFixZoomInZoomOutDiscrepancy then
+            if (zoomDelta > 0) then
+                newZoom = zoom + zoomDelta*(1 + zoom*ZOOM_SENSITIVITY_CURVATURE)
+            else
+                newZoom = (zoom + zoomDelta) / (1 - zoomDelta*ZOOM_SENSITIVITY_CURVATURE)
+            end
+        else
+            newZoom = zoom + zoomDelta*(1 + zoom*ZOOM_SENSITIVITY_CURVATURE)
+        end
+
+        zoom = math.clamp(newZoom, player.CameraMinZoomDistance, player.CameraMaxZoomDistance)
+    end
 end
 
 return CameraController
